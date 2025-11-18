6ed48e7f136b7307404c1ffb4d3ea3e17cc27498
superwhys <hovenyang.best@gmail.com>
2025-11-18 17:43:13 +0800

    update: init project

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"github.com/miebyte/goutils/cores"
+	"github.com/miebyte/goutils/flags"
+	"github.com/miebyte/goutils/logging"
+)
 
+var (
+	port = flags.Int("port", 8080, "server run port")
+)
+
+func main() {
+	flags.Parse()
+
+	srv := cores.NewCores()
+
+	logging.PanicError(cores.Start(srv, port()))
 }
